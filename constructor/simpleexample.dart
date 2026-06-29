17daf85a1d7e5a4a0851ac25379719cc5360c846
shaharyar <shaharyar4t4@gmail.com>
2026-06-29 15:27:15 +0500

    learing about the constructor...

---
+++ constructor/simpleexample.dart
@@ -0,0 +1,11 @@
+class Simpleexample {
+  Simpleexample() {
+    print("Laptop created..");
+  }
+}
+
+void main() {
+  Simpleexample expone = Simpleexample();
+  Simpleexample exptwo = Simpleexample();
+
+}
