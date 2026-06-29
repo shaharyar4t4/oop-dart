5d54e604e0af6796012c9a672bd4000df7493319
shaharyar <shaharyar4t4@gmail.com>
2026-06-29 11:56:19 +0500

    initial commit

---
+++ class-object/sampleclassandobject.dart
@@ -0,0 +1,16 @@
+class Sampleclassandobject {
+  
+  // variable is store on 
+  // this is properties of any student..
+  String name = "Shaharyar";
+  String phoneno = "03879595959";
+
+  // this is behaviour of any student..
+  void student(){
+    print("this is student detial...");
+  }
+}
+
+  void main(){
+    
+  }
